a2e82b6854e2821eec88f628bc674a33f6edb981
ogane98 <ogane98@naver.com>
2025-09-13 23:15:43 +0900

    chore: 축 범위 수정, 경로 최대 길이 설정

--- src/pcl_processor/pcl_processor/odometry_visualizer.py
+++ src/pcl_processor/pcl_processor/odometry_visualizer.py
@@ -14,6 +14,7 @@ class OdometryVisualizer(Node):
             10)
         self.x_data = []
         self.y_data = []
+        self.max_path_length = 100
 
         plt.ion()
         self.fig, self.ax = plt.subplots()
@@ -22,6 +23,8 @@ class OdometryVisualizer(Node):
         self.ax.set_ylabel('Y Position')
         self.ax.set_title('Odometry Path')
         self.ax.grid(True)
+        self.ax.set_xlim(-10, 10)
+        self.ax.set_ylim(-10, 10)
         
         self.timer = self.create_timer(0.1, self.update_plot)
 
@@ -31,11 +34,13 @@ class OdometryVisualizer(Node):
         self.x_data.append(x)
         self.y_data.append(y)
 
+        if len(self.x_data) > self.max_path_length:
+            self.x_data.pop(0)
+            self.y_data.pop(0)
+
     def update_plot(self):
         self.line.set_xdata(self.x_data)
         self.line.set_ydata(self.y_data)
-        self.ax.relim()
-        self.ax.autoscale_view()
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
